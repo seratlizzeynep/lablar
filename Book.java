45ec80147d0427ec34c1af3b01589924d78bb910
seratlizzeynep <zeynepaybuke.seratli@tedu.edu.tr>
2024-10-15 19:16:50 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,63 @@
+package lab012;
+
+
+public class Book {
+
+private int releaseDate;
+private String name;
+private double price;
+
+
+public Book(int relDate, String nm, double prc) {
+    this.releaseDate = relDate;
+    this.name = nm;
+    this.price = prc;
+}
+
+public int getReleaseDate() {
+    return releaseDate;
+}
+
+public void setReleaseDate(int releaseDate) {
+    this.releaseDate = releaseDate;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public double getPrice() {
+    return price;
+}
+
+public void setPrice(double price) {
+    this.price = price;
+}
+
+
+public void subtract(double amount) {
+    this.price = this.price - amount;
+}
+
+
+public String toString() {
+    return "Name: " + this.name + " Release Date: " + this.releaseDate + " Price: " + this.price;
+}
+
+
+public double deductPrice() {
+    this.price = this.price -  50;
+    return this.price;
+}
+
+
+public void changeName(String newName) {
+    this.name = newName;
+
+
+}
+}
